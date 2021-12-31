a9ddd5b8473fd57edaf22f47720a0df99e0c4884
stas <bondarenko.stanislav.96@gmail.com>
2021-12-31 03:45:56 +0200

    day 6

---
+++ 2021/day6/1.rb
@@ -0,0 +1,41 @@
+def read_input(filename)
+	File.readlines(filename)[0].split(',').map(&:to_i)
+end
+
+class Fish
+  def initialize(days = 8)
+    @days = days
+  end
+
+  def should_spawn?
+		should_spawn = @days == 0
+		@days = @days == 0 ? 6 : @days - 1
+		should_spawn
+  end
+end
+
+class School
+  def initialize(days_array)
+    @old_fish = days_array.map { |days_left| Fish.new(days_left) }
+	end
+
+	def increment_day
+		new_fish = []
+		@old_fish.each { |fish| new_fish.append(Fish.new) if fish.should_spawn? }
+		@old_fish += new_fish
+	end
+
+	def increment_days(days)
+		days.times { increment_day }
+		self
+	end
+
+	def count
+		@old_fish.count
+	end
+end
+
+# days_array = read_input('./input')
+days_array = read_input('./try_input')
+
+p School.new(days_array).increment_days(80).count
---
+++ 2021/day6/2.rb
@@ -0,0 +1,39 @@
+def read_input(filename)
+	File.readlines(filename)[0].split(',').map(&:to_i)
+end
+
+class School
+  def initialize(days_array)
+		@fish = {}
+		@new_fish = 0
+		days_array.each { |days| @fish[days] = (@fish[days] || 0) + 1 }
+	end
+
+	def increment_day
+    new_count = {8 => @new_fish}
+    @new_fish = 0
+        
+    @fish.each { |days, count| new_count[days - 1] = count }
+		@new_fish = new_count[0] if new_count[0]
+
+    if new_count[-1]
+			new_count[6] = (new_count[6] || 0) + new_count[-1]
+      new_count.delete(-1)
+    end
+    @fish = new_count
+	end
+
+	def increment_days(days)
+		days.times { increment_day }
+		self
+	end
+
+	def count
+		@fish.values.sum
+	end
+end
+
+days_array = read_input('./input')
+# days_array = read_input('./try_input')
+
+p School.new(days_array).increment_days(256).count
